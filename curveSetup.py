05175926eb9a7a349249724aa571d11f2230d9bb
Unknown <joseph@josephmasaki.com>
2018-04-01 20:37:24 -0700

    Finished removing C, D, O

--- curveSetup.py
+++ curveSetup.py
@@ -1,18 +1,11 @@
-#----------------------------------------------------------
-# File mycube.py
-#----------------------------------------------------------
-
 import bpy
 import random
 from mathutils import Vector
-C=bpy.context
-D=bpy.data
-O=bpy.ops
 
 
 def curveSetup (curve, repeater, spacing, lanes=0):
     
-    mySpline=D.curves[curve.data.name].splines.active
+    mySpline=bpy.data.curves[curve.data.name].splines.active
     mySpline.bezier_points[0].co.xyz=Vector((0,0,0))                        
     mySpline.bezier_points[1].co.xyz=Vector((0,100,0))
 
@@ -20,7 +13,7 @@ def curveSetup (curve, repeater, spacing, lanes=0):
         if i == 0:
             mySpline.bezier_points[i].handle_left_type = 'VECTOR'
             mySpline.bezier_points[i].handle_right_type = 'VECTOR'
-        else: 
+        else: z
             mySpline.bezier_points[i].handle_left_type = 'ALIGNED'
             mySpline.bezier_points[i].handle_right_type = 'ALIGNED'
             mySpline.bezier_points[i].handle_left = (50, 75, 0)
@@ -29,23 +22,23 @@ def curveSetup (curve, repeater, spacing, lanes=0):
 
 
     
-    D.objects[repeater.name].parent=curve
-    C.scene.objects.active = repeater
+    bpy.data.objects[repeater.name].parent=curve
+    bpy.context.scene.objects.active = repeater
 
 
 
 
     ## of lanes across the highway
     if repeater.name =="lanes":
-        D.objects[repeater.name].modifiers.new("ArrayLanes", type='ARRAY')
-        D.objects[repeater.name].modifiers["ArrayLanes"].count=lanes
-        D.objects[repeater.name].modifiers["ArrayLanes"].relative_offset_displace=(0,1,0)
+        bpy.data.objects[repeater.name].modifiers.new("ArrayLanes", type='ARRAY')
+        bpy.data.objects[repeater.name].modifiers["ArrayLanes"].count=lanes
+        bpy.data.objects[repeater.name].modifiers["ArrayLanes"].relative_offset_displace=(0,1,0)
 
 
 
     #mirror, since these are 2 way symmetrical roads
     repeater.modifiers.new("Mirror_Twoway", type='MIRROR')
-    D.objects[repeater.name].modifiers["Mirror_Twoway"].use_x=False
+    bpy.data.objects[repeater.name].modifiers["Mirror_Twoway"].use_x=False
     repeater.modifiers["Mirror_Twoway"].use_y=True #identical, left in to show goal
     
 
@@ -62,8 +55,8 @@ def curveSetup (curve, repeater, spacing, lanes=0):
     repeater.modifiers.new("Curve", type='CURVE')
     repeater.modifiers["Curve"].object=curve
     
-    C.scene.objects.active = repeater
-    O.object.location_clear(clear_delta=False)
+    bpy.context.scene.objects.active = repeater
+    bpy.ops.object.location_clear(clear_delta=False)
 
 
     #Visibility
